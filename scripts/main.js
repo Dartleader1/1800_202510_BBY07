a355d205bde46c3b527d5686b90e8e5cc2535a00
Justin Chik <justinrchik@gmail.com>
2025-03-12 11:44:33 -0700

    Added lend page with form to submit battery info. made javascript to read and write this data to firestore.

--- scripts/main.js
+++ scripts/main.js
@@ -7,12 +7,34 @@ function writeBattery() {
         name: "Burnaby Lake Park Trail", //replace with your own city?
         city: "Burnaby",
         province: "BC",
-        level: "easy",
 				details: "A lovely place for lunch walk",
-        length: 10,          //number value
-        hike_time: 60,       //number value
         lat: 49.2467097082573,
         lng: -122.9187029619698,
         last_updated: firebase.firestore.FieldValue.serverTimestamp()  //current system time
+    });
+}
+function getNameFromAuth() {
+  firebase.auth().onAuthStateChanged(user => {
+      // Check if a user is signed in:
+      if (user) {
+          // Do something for the currently logged-in user here: 
+          console.log("user id: " + user.uid); //print the uid in the browser console
+          console.log("user name: " + user.displayName);  //print the user name in the browser console
+          userName = user.displayName;
+
+          document.getElementById("name-goes-here").innerText = userName;    
+      } else {
+          // No user is signed in.
+          console.log ("No user is logged in");
+      }
+  });
+}
+getNameFromAuth();
+function logout() {
+  firebase.auth().signOut().then(() => {
+      // Sign-out successful.
+      console.log("logging out user");
+    }).catch((error) => {
+      // An error happened.
     });
 }
---
+++ scripts/savebattery.js
@@ -0,0 +1,29 @@
+function saveBattery() {
+    console.log("Saving battery for user");
+    let batteryName = document.getElementById("batteryName").value;
+    // let batteryPhoto = document.getElementById("batteryPhoto").value;
+    let batteryCable = document.getElementById("batteryCable").value;
+    let batteryPort = document.getElementById("batteryPort").value;
+    let batteryCapacity = document.getElementById("batteryCapacity").value;
+
+console.log(batteryName, batteryCable, batteryPort, batteryCapacity);
+
+var user = firebase.auth().currentUser;
+    if (user) {
+        var currentUser = db.collection("users").doc(user.uid);
+        var userID = user.uid;
+
+        // Get the document for the current user.
+        db.collection("batteries").add({
+            batteryName: batteryName,
+            // batteryPhoto: batteryPhoto,
+            batteryCable: batteryCable,
+            batteryPort: batteryPort,
+            batteryCapacity: batteryCapacity,
+            userID: userID,
+            timestamp: firebase.firestore.FieldValue.serverTimestamp()
+        }).then(() => {
+            window.location.href = "main.html"; // Redirect to main page
+        });
+}
+}
